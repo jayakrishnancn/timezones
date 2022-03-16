6810cd1aa331fff661095607b0b32d5471c116d1
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-03-16 18:12:44 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [startTimes, setSetStartTimes] = useState<number[]>([0, 5.5]);
+
+  const shiftTimeWithOffset = useCallback((offset: number): string[] => {
+    const clock: string[] = [];
+    for (let i = 1; i <= 24; i++) {
+      let time = i + offset;
+      const amPm = time < 12 ? "AM" : "PM";
+      time = time > 12 ? time % 12 : time;
+      clock.push(`${time || 12} ${amPm}`);
+      time += 0.5;
+      time = time > 12 ? time % 12 : time;
+      clock.push(`${time || 12} ${amPm}`);
+    }
+    return clock;
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setSetStartTimes((prev) => [...prev, 5.5])}>
+        ADD
+      </button>
+      <table className="table">
+        {startTimes.map((startTime, index) => (
+          <tr>
+            <input
+              value={startTime}
+              step=".5"
+              type="number"
+              className="timeinput"
+              onChange={(e) => {
+                const val = Number(e.target.value);
+                setSetStartTimes((prev) => {
+                  prev[index] = val;
+                  return [...prev];
+                });
+              }}
+            />
+            {shiftTimeWithOffset(startTime).map((time, index) => (
+              <td key={index}>{time}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }

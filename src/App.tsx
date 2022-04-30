b7a7578c53799fb4f32259ecb4a5a63a878f350c
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-04-30 22:42:11 +0530

    timezones added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import Select from "react-select";
 import "./App.css";
+import { timeZones } from "./utils/timezones";
 
 function App() {
-  const [startTimes, setSetStartTimes] = useState<number[]>([0, 5.5]);
+  const [startTimes, setSetStartTimes] = useState<number[]>(
+    JSON.parse(localStorage.getItem("saved") ?? "[37]")
+  );
 
   const shiftTimeWithOffset = useCallback((offset: number): string[] => {
     const clock: string[] = [];
@@ -11,39 +15,60 @@ function App() {
       const amPm = time < 12 ? "AM" : "PM";
       time = time > 12 ? time % 12 : time;
       clock.push(`${time || 12} ${amPm}`);
-      time += 0.5;
+      /* time += 0.5;
       time = time > 12 ? time % 12 : time;
-      clock.push(`${time || 12} ${amPm}`);
+      clock.push(`${time || 12} ${amPm}`); */
     }
     return clock;
   }, []);
 
+  useEffect(() => {
+    startTimes && localStorage.setItem("saved", JSON.stringify(startTimes));
+  }, [startTimes]);
+
+  const TIME_ZONES = timeZones.map((item, value) => ({
+    label: item.abbr + item.text,
+    value,
+    offset: item.offset,
+  }));
+  const LONDON_TZ_ID = 37;
   return (
     <div className="App">
-      <button onClick={() => setSetStartTimes((prev) => [...prev, 5.5])}>
+      <button
+        onClick={() => setSetStartTimes((prev) => [...prev, LONDON_TZ_ID])}
+      >
         ADD
       </button>
+
       <table className="table">
-        {startTimes.map((startTime, index) => (
-          <tr>
-            <input
-              value={startTime}
-              step=".5"
-              type="number"
-              className="timeinput"
-              onChange={(e) => {
-                const val = Number(e.target.value);
-                setSetStartTimes((prev) => {
-                  prev[index] = val;
-                  return [...prev];
-                });
-              }}
-            />
-            {shiftTimeWithOffset(startTime).map((time, index) => (
-              <td key={index}>{time}</td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {startTimes.map((startTime, index) => (
+            <tr key={startTime}>
+              <td>
+                <Select
+                  onChange={(option) => {
+                    option !== null &&
+                      setSetStartTimes((prev) => {
+                        prev[index] = option?.value;
+                        return [...prev];
+                      });
+                  }}
+                  value={TIME_ZONES.find(
+                    (item) => item.value === startTimes[index]
+                  )}
+                  options={TIME_ZONES}
+                />
+              </td>
+              {shiftTimeWithOffset(TIME_ZONES[startTime].offset).map(
+                (time, index) => (
+                  <td className="time" key={index}>
+                    {time}
+                  </td>
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
